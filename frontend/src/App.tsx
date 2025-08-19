9197ee0a63c21234a59e587d264ca3604c667ea3
alok-raj <Alok.Raj@mindfiresolutions.com>
2025-08-19 19:38:43 +0530

    feat(client): add error boundry for frontent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
@@ -9,7 +9,8 @@ import { RegisterForm } from './components/auth/RegisterForm';
 import { Toaster } from "@/components/ui/sonner"
 import Transactions from './pages/Transactions';
 import CategoryPage from './pages/Category';
-
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from './components/ErrorFallBack';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -24,13 +25,13 @@ const AppRoutes: React.FC = () => {
 
   return (
     <Routes>
-      <Route 
-        path="/login" 
-        element={isAuthenticated ? <Navigate to="/" replace /> : <LoginForm />} 
+      <Route
+        path="/login"
+        element={isAuthenticated ? <Navigate to="/" replace /> : <LoginForm />}
       />
-      <Route 
-        path="/register" 
-        element={isAuthenticated ? <Navigate to="/" replace /> : <RegisterForm />} 
+      <Route
+        path="/register"
+        element={isAuthenticated ? <Navigate to="/" replace /> : <RegisterForm />}
       />
       <Route
         path="/"
@@ -49,12 +50,17 @@ const AppRoutes: React.FC = () => {
   );
 };
 
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Router>
-          <AppRoutes />
+          <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+          >
+            <AppRoutes />
+          </ErrorBoundary>
           <Toaster />
         </Router>
       </AuthProvider>
